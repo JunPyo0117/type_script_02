9b626929ce8b2adc39d00384749a47b5b686d40c
JunPyo0117 <vksehfkdydy@naver.com>
2025-12-23 14:51:10 +0900

    Add OOP principles (SOLID) to Readme, detailing SRP, OCP, LSP, ISP, and DIP

--- Readme.md
+++ Readme.md
@@ -5,4 +5,10 @@
 3. 상속(inherytance) > extends, abstract
 4. 다형성(polymorphism) > extends, override
 
-  
+- OOP 객체지향 설계의 5원칙 SOLID
+
+1. SRP(Single Responsibility Principle) 단일 책임 원칙
+2. OCP(Open Closed Priciple) 개방 쳬쇄 원칙
+3. LSP(Listov Substitution Priciple) 리스코프 치환원칙
+4. ISP(Interface Segregation Principle) 인터페이스 분리 원칙
+5. DIP(Dependency Inversion Principle) 의존 역전 원칙
---
+++ solid.ts
@@ -0,0 +1,82 @@
+// 바로 구현해서 씀
+class DatabaseConnection {
+
+    constructor(
+        private readonly IP,
+        private readonly PORT,
+        private readonly ID,
+        private readonly PASSWORD,
+    ) {}
+
+    // 연결과 설정 관련 메서드
+    connect() {
+        console.log("connect to database");
+
+        return "연결 후 객체"
+    }
+
+    disconnect() {
+        console.log("disconnect from database");
+    }
+}
+
+interface IRepository {
+    save();
+    findMany();
+    findOneID();
+    update();
+    delete();
+}
+
+
+class MongoRepository implements IRepository {
+    constructor(private readonly connection: DatabaseConnection) {}
+    // CRUD Create, Read, Update, Delete
+    save() {
+        console.log("save data to Mongo database");
+    }
+
+    findMany() {
+        console.log("find many data from Mongo database");
+    }
+
+    findOneID() {
+        console.log("find one data from Mongo database by ID");
+    }
+
+    update() {
+        console.log("update data in Mongo database");
+    }
+
+    delete() {
+        console.log("delete data from Mongo database");
+    }
+}
+
+class MYSQLRepository implements IRepository {
+    constructor(private readonly connection: DatabaseConnection) {}
+    // CRUD Create, Read, Update, Delete
+    save() {
+        console.log("save data to MYSQL database");
+    }
+
+    findMany() {
+        console.log("find many data from MYSQL database");
+    }
+
+    findOneID() {
+        console.log("find one data from MYSQL database by ID");
+    }
+
+    update() {
+        console.log("update data in MYSQL database");
+    }
+
+    delete() {
+        console.log("delete data from MYSQL database");
+    }
+}
+
+// 백엔드
+const repo1: IRepository = new MongoRepository(new DatabaseConnection("127.0.0.1", 27017, "root", "password"));
+const repo2: IRepository = new MYSQLRepository(new DatabaseConnection("127.0.0.1", 27017, "root", "password"));
